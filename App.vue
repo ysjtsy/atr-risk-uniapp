cf331e5aa732fb651b694eb9d28c5ef9c35295ba
qszOrangebro <129163154+ysjtsy@users.noreply.github.com>
2025-11-15 22:21:11 +0800

    chore: bootstrap vue3 config

--- App.vue
+++ App.vue
@@ -1,28 +1,20 @@
-<script>
-// app.js
-export default {
-    data() {
-        return {};
-    },
-    onLaunch() {
-        // 展示本地存储能力
-        const logs = uni.getStorageSync('logs') || [];
-        logs.unshift(Date.now());
-        uni.setStorageSync('logs', logs);
+<script setup>
+import { onLaunch } from '@dcloudio/uni-app';
 
-        // 登录
-        uni.login({
-            success: (res) => {
-                // 发送 res.code 到后台换取 openId, sessionKey, unionId
-            }
-        });
-    },
-    globalData: {
-        userInfo: null
+onLaunch(() => {
+  const logs = uni.getStorageSync('logs') || [];
+  logs.unshift(Date.now());
+  uni.setStorageSync('logs', logs);
+
+  uni.login({
+    success: () => {
+      // 发送 res.code 到后台换取 openId, sessionKey, unionId
     }
-};
+  });
+});
 </script>
+
 <style>
 @import './styles/variables.css';
-@import './styles/base.css'; /**app.wxss**/
+@import './styles/base.css';
 </style>
